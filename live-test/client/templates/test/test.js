5809ed2622775ee4998d3d31dc0bd4a0ff82a970
Abzal Serekov <abzal.serekov@gmail.com>
2015-10-28 23:01:57 +0600

    latex stuff is done

--- live-test/client/templates/test/test.js
+++ live-test/client/templates/test/test.js
@@ -8,6 +8,22 @@ Template.test.rendered = function () {
 
 }
 
+function katexify(s) {
+	var p = "";
+	var cnt = 0;
+	for (var i = 0; i < s.length; i++) {
+		if (s[i] == '$') {
+			if (cnt % 2 == 0)
+				p += "{{#katex}}$";
+			else
+				p += "${{/katex}}";
+			cnt++;
+		} else
+			p += s[i];
+	}
+	return p;
+}
+
 Template.test.helpers({
 	kazakh_questions: function () {
 		return Questions.find({course_en: 'kazakh'});
@@ -16,7 +32,17 @@ Template.test.helpers({
 		return Questions.find({course_en: 'russian'});
 	},
 	math_questions: function () {
-		return Questions.find({course_en: 'math'});
+		var my_questions = Questions.find({course_en: 'math'});
+		for (var i = 0; i < my_questions.length; i++) {
+			my_questions[i].question = katexify(my_questions[i].question);
+			my_questions[i].choice_A = katexify(my_questions[i].choice_A);
+			my_questions[i].choice_B = katexify(my_questions[i].choice_B);
+			my_questions[i].choice_C = katexify(my_questions[i].choice_C);
+			my_questions[i].choice_D = katexify(my_questions[i].choice_D);
+			my_questions[i].choice_E = katexify(my_questions[i].choice_E);
+		}
+		return my_questions;
+
 	},
 	history_questions: function () {
 		return Questions.find({course_en: 'history'});
@@ -24,6 +50,9 @@ Template.test.helpers({
 	fifth_questions: function () {
 		return Questions.find({is_fifth: true, course_en: 'physics'});
 	},
+	katexify: function (s) {
+		return katexify(s);
+	}
 });
 
 Template.test.events({
